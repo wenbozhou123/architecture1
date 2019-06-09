31c9e4a74a53dedc3cd680dcb676327fca135bb0
wenbozhou123 <18721789537@163.com>
2019-06-10 00:44:14 +0800

    refactor customer modules

---
+++ common/src/main/java/com/bowen/architecture/architecture1/common/dao/BaseDAO.java
@@ -0,0 +1,12 @@
+package com.bowen.architecture.architecture1.common.dao;
+
+import java.util.List;
+
+public interface BaseDAO<M, QM> {
+    public void create(M cm);
+    public void update(M cm);
+    public void delete(int uuid);
+
+    public M getByUuid(int uuid);
+    public List<M> getByConditionPage(QM qm);
+}
--- customerMgr/src/main/java/com/bowen/architecture/architecture1/customerMgr/dao/CustomerDAO.java
+++ customerMgr/src/main/java/com/bowen/architecture/architecture1/customerMgr/dao/CustomerDAO.java
@@ -1,19 +1,12 @@
 package com.bowen.architecture.architecture1.customerMgr.dao;
 
+import com.bowen.architecture.architecture1.common.dao.BaseDAO;
 import com.bowen.architecture.architecture1.customerMgr.vo.CustomerModel;
 import com.bowen.architecture.architecture1.customerMgr.vo.CustomerQueryModel;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
-public interface CustomerDAO {
-
-    public void create(CustomerModel cm);
-    public void update(CustomerModel cm);
-    public void delete(int uuid);
-
-    public CustomerModel getByUuid(int uuid);
-    public List<CustomerModel> getByConditionPage(CustomerQueryModel cqm);
+public interface CustomerDAO extends BaseDAO<CustomerModel, CustomerQueryModel> {
 
 }
