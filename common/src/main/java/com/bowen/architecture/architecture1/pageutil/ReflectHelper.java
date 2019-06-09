0f61bc8b4b96e57685b5c015df5764af2b5d44e7
wenbozhou123 <18721789537@163.com>
2019-06-10 00:35:10 +0800

    paging customer

--- common/src/main/java/com/bowen/architecture/architecture1/pageutil/ReflectHelper.java
+++ common/src/main/java/com/bowen/architecture/architecture1/pageutil/ReflectHelper.java
@@ -10,12 +10,12 @@ public class ReflectHelper {
      * @param fieldName
      * @return
      */
-    public static Field getFieldByFieldName(Object obj, String fieldName) {
-        for (Class<?> superClass = obj.getClass(); superClass != Object.class; superClass = superClass
-                .getSuperclass()) {
+    public static Field getFieldByFieldName(Object obj, String fieldName){
+        for(Class<?> superClass = obj.getClass(); superClass != Object.class; superClass = superClass.getSuperclass()){
             try {
                 return superClass.getDeclaredField(fieldName);
             } catch (NoSuchFieldException e) {
+
             }
         }
         return null;
@@ -31,15 +31,13 @@ public class ReflectHelper {
      * @throws IllegalArgumentException
      * @throws IllegalAccessException
      */
-    public static Object getValueByFieldName(Object obj, String fieldName)
-            throws SecurityException, NoSuchFieldException,
-            IllegalArgumentException, IllegalAccessException {
+    public static Object getValueByFieldName(Object obj, String fieldName) throws IllegalAccessException {
         Field field = getFieldByFieldName(obj, fieldName);
         Object value = null;
-        if(field!=null){
-            if (field.isAccessible()) {
+        if(field != null){
+            if (field.isAccessible()){
                 value = field.get(obj);
-            } else {
+            }else {
                 field.setAccessible(true);
                 value = field.get(obj);
                 field.setAccessible(false);
@@ -48,31 +46,22 @@ public class ReflectHelper {
         return value;
     }
 
-    /**
-     * 设置obj对象fieldName的属性值
-     * @param obj
-     * @param fieldName
-     * @param value
-     * @throws SecurityException
-     * @throws NoSuchFieldException
-     * @throws IllegalArgumentException
-     * @throws IllegalAccessException
-     */
-    public static void setValueByFieldName(Object obj, String fieldName,
-                                           Object value) throws SecurityException, NoSuchFieldException,
-            IllegalArgumentException, IllegalAccessException {
+    public static void setValueByFieldName(Object obj, String fieldName, Object value)
+            throws NoSuchFieldException, IllegalAccessException {
+
         try{
-            Field field = obj.getClass().getDeclaredField(fieldName);
-            if (field.isAccessible()) {
+            Field  field = obj.getClass().getDeclaredField(fieldName);
+            if(field.isAccessible()){
                 field.set(obj, value);
-            } else {
+            }else {
                 field.setAccessible(true);
                 field.set(obj, value);
                 field.setAccessible(false);
             }
-        }catch(Exception err){
-            //
+        }catch (Exception e){
+
         }
+
     }
 
 
