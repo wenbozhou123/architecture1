bdcf6c20ebd1d5f2f84bdd177039e42942fc6f1f
wenbozhou123 <18721789537@163.com>
2019-06-11 00:24:29 +0800

    coding controller

--- common/src/main/java/com/bowen/architecture/architecture1/util/json/JsonHelper.java
+++ common/src/main/java/com/bowen/architecture/architecture1/util/json/JsonHelper.java
@@ -1,13 +1,21 @@
 package com.bowen.architecture.architecture1.util.json;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 public class JsonHelper {
 
     private JsonHelper(){}
 
-    public static String object2str(){
-        //Jackson2ObjectMapperBuilder
-        return "";
+    public static String object2str(Object obj){
+        String reStr = "";
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            reStr = mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return reStr;
     }
 }
